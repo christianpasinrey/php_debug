c6e48b2d6a99b2453c76c75647135332fa6cef76
Christian Pasín Rey <christian.pasin.rey@gmail.com>
2024-07-13 18:17:07 +0200

    Debug class

--- src/dd.php
+++ src/dd.php
@@ -5,31 +5,37 @@ namespace Chris\Src;
 use function Chris\Src\Handlers\displayWarning;
 use function Chris\Src\Handlers\varDump;
 
-set_error_handler('customErrorHandler');
-
-function dd(...$vars)
+class Debug
 {
-    ob_start();
-    echo file_get_contents("./assets/partials/head.html");
-    echo '<div class="dd-container">';
+    public function __construct()
+    {
+        set_error_handler('Chris\Src\Handlers\customErrorHandler');
+    }
 
-    if (!empty($GLOBALS['warnings'])) {
-        foreach ($GLOBALS['warnings'] as $warning) {
-            displayWarning($warning);
+    public static function dd(...$vars)
+    {
+        ob_start();
+        echo file_get_contents("./assets/partials/head.html");
+        echo '<div class="dd-container">';
+
+        if (!empty($GLOBALS['warnings'])) {
+            foreach ($GLOBALS['warnings'] as $warning) {
+                displayWarning($warning);
+            }
         }
-    }
 
-    foreach ($vars as $var) {
-        varDump($var);
-    }
+        foreach ($vars as $var) {
+            varDump($var);
+        }
 
-    echo '</div>';
+        echo '</div>';
 
-    echo file_get_contents("./assets/partials/foot.html");
-    $output = ob_get_clean();
-    while (ob_get_level()) {
-        ob_end_clean();
+        echo file_get_contents("./assets/partials/foot.html");
+        $output = ob_get_clean();
+        while (ob_get_level()) {
+            ob_end_clean();
+        }
+        echo $output;
+        exit;
     }
-    echo $output;
-    exit;
 }
