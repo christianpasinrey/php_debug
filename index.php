9e9715906d9a6f745c17c5be08c873a767ae5495
Christian Pasín Rey <christian.pasin.rey@gmail.com>
2024-07-13 17:27:59 +0200

    Deleted User class to start refactoring and improving concept

--- index.php
+++ index.php
@@ -18,46 +18,9 @@
         require_once('User.php');
         include('debug.php');
 
-        //session_start();
-        //dd($_POST, $_SESSION, $_GET, $_FILES, $_COOKIE, $_SERVER);
 
-        try {
-            $data = [
-                'name' => 'John Doe',
-                'email' => 'johndoe@example.net',
-                'age' => 30,
-                //'isStudent' => false,
-                'address' => [
-                    'street' => '123 Main St',
-                    'city' => 'Springfield',
-                    'state' => 'IL',
-                    'zip' => '62701'
-                ],
-                'hobbies' => ['reading', 'swimming', 'coding'],
-            ];
-            $user = new User($data);
-            //$error = createUser('John Doe', 30);
-            //dd($user->__toString());
-            dd($user);
-        } catch (Exception $e) {
-            $data = [
-                'error' => $e->getMessage(),
-                'file' => $e->getFile(),
-                'line' => $e->getLine(),
-                'trace' => $e->getTraceAsString()
-            ];
-            // Capturamos Exception para atrapar excepciones definidas
-            dd($data);
-        } catch (Error $e) {
-            $data = [
-                'error' => $e->getMessage(),
-                'file' => $e->getFile(),
-                'line' => $e->getLine(),
-                'trace' => $e->getTraceAsString()
-            ];
-            // Capturamos Error en lugar de Exception para atrapar errores de función indefinida
-            dd($data);
-        }
+        dd();
+
         ?>
     </pre>
 </body>
